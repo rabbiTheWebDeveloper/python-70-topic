04f980605ae4ba46a55b4634c2a0eb261ed560f2
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-01 12:51:02 +0600

    adding basic info

---
+++ fourth.py
@@ -0,0 +1,26 @@
+# comment how to write comment in python 
+# single line comment
+# '''           
+# multi line comment
+# '''
+
+a=15
+b=2
+print(a+b) # addition
+print(a-b)# subtraction
+print(a*b)  # multiplication
+print(a/b) # division
+print(a%b) # modulus
+print("floor division ",a//b) # floor division
+print(a**b)  # a^b
+# True or False 
+print(a>b) # greater than
+print(a<b) # less than
+print(a>=b) # greater than or equal to
+print(a<=b) # less than or equal to 
+print(a==b) # equal to
+print(a!=b) # not equal to
+print((a>b) and (a<b)) # and operator
+print((a>b) or (a<b)) # or operator
+print(not(a>b)) # not operator
+# assignment operator
