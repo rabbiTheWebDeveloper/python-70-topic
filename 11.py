691c569e41bb46b0676b88201bc990a54a705332
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-01 21:04:35 +0600

    11 adding problem solving
    11 adding problem solving

---
+++ 11.py
@@ -0,0 +1,10 @@
+mark=input("Enter your mark : ")
+mark=int(mark)
+if mark > 90 :
+    print("A")
+if mark > 80  and mark <= 90:
+    print("B")
+if mark > 70 and mark <= 80:
+    print("C")
+else:
+    print("D")
