8fe84c5436fa95d57523f1e9e9d92943e3290986
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-01 20:55:29 +0600

    adding the problem 9 solution
    adding the problem 9 solution

---
+++ 9.py
@@ -0,0 +1,10 @@
+a=input("Enter the Value of a : ")
+b=input ("Enter the Value of b : ")
+c=input("Enter the Value of c : ")
+
+if(a>b) and (a>c):
+    print("a is the greatest")
+elif(b>c) and (b>a):
+    print("b is the greatest")
+else:
+    print("c is the greatest")
