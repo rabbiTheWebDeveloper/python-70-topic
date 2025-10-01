9daf268b530d637d0abfc7f1546906b4c1f5b35b
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-01 21:38:08 +0600

    adding while loop

---
+++ 13.py
@@ -0,0 +1,21 @@
+# introdaction of loop 
+# type of loop
+# for loop
+# while loop
+# nested loop
+name="I LOVE Python"
+
+# for i in name:
+#     print(i)
+# for item in range(1+10) :
+#         print( item ,"I love python")
+
+a  =list( range(10))
+b= list(range(0,11 ,3))
+print(a)
+print(b)
+
+sum =0
+for i in range(1,11):
+    sum = sum + i
+print("The sum is ",sum)
---
+++ 14.py
@@ -0,0 +1,4 @@
+a=0
+while a<10:
+    print("This is while loop ",a)
+    a+=1
