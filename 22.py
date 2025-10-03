148e03282cc0a7bc9e7901fc89a29d87b10cd325
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-03 07:39:42 +0600

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,12 @@
+#longcut method 
+a=int(input("Enter The Number : "))
+count =0
+
+while a>0 :
+    count=count+1
+    a=a//10
+print(count)
+
+# method 2 short cut method 
+b=input()
+print(len(b))
