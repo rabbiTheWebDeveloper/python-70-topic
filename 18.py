41934f506afc9e0c2115100f3cf3ff0400f7afd3
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-03 07:14:10 +0600

    17 problem
    17 problem

---
+++ 18.py
@@ -0,0 +1,5 @@
+list_product=[[12,15,25] ,["apple" ,"orange"],[True,False]]
+for item in list_product :
+    print(item)
+    for get_product in item :
+        print(get_product)
---
+++ 19.py
@@ -0,0 +1,4 @@
+a=int(input("Enter The Value : "))
+
+for i in range(a+1):
+    print(i ,"*" ,a ,"=" ,a*i)
