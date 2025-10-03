f0a525cf3f44e42060e8c064e2ec8c08627beed8
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-10-03 07:32:08 +0600

    febonasce

---
+++ 20.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the Value : " , ))
+sum=1
+for i in range(1,a+1):
+    sum=sum*i
+print("Factorial OF Numberr" ,sum)
---
+++ 21.py
@@ -0,0 +1,8 @@
+a=0
+b=1
+# a,b=0,1
+for i in range(10):
+    print(a , end=" ")
+    result = a+b
+    a=b
+    b=result
